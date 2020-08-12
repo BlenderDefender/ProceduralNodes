2845fb65f0df812602e25b9d1ac45002144a8a8f
Blender Defender <contact.blenderdefender@gmail.com>
2020-08-12 17:43:47 +0200

    Improve the filebrowser test

--- Test Filebrowser.py
+++ Test Filebrowser.py
@@ -9,25 +9,17 @@ import os
 class OT_TestOpenFilebrowser(Operator, ImportHelper):
 
     bl_idname = "test.open_filebrowser"
-    bl_label = "Open the file browser (yay)"
-
-#    filter_glob: StringProperty(
-#        default='*.jpg;*.jpeg;*.png;*.tif;*.tiff;*.bmp; *.blend',
-#        options={'HIDDEN'} )
-
-#    some_boolean: BoolProperty(
-#        name='Do a thing',
-#        description='Do a thing with the file you\'ve selected',
-#        default=True, )
+    bl_label = "Install"
 
     def execute(self, context):
         """Do something with the selected file(s)."""
 
-        filename, extension = os.path.splitext(self.filepath)
+        filename = os.path.basename(self.filepath)
+        extension = os.path.splitext(filename)[1]
+
         print('Selected file:', self.filepath)
         print('File name:', filename)
         print('File extension:', extension)
-#        print('Some Boolean:', self.some_boolean)
 
         return {'FINISHED'}
 
